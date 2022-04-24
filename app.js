bb7e222f85f654ff77489ac9be938f103ea080f2
JohnNadja <cv3802@innovaccion.mx>
2022-04-23 22:45:15 -0500

    4: Adding new rute to get an object in the server

--- app.js
+++ app.js
@@ -6,10 +6,25 @@ app.get('/', (req, res) => {
     res.send("Hola a todos mis explorers")
 })
 
+// path inicial, respondereá a la url localhost:3000}
 app.listen(port, () =>{
     console.log("¡Server Listo!")
 })
 
+
+//Respondiendo a un texto
+// localhost:3000/launchx
 app.get('/launchx', (req, res) =>{
     res.send('Bienvenidos a launchx')
 })
+
+
+// Regresando un objeto
+// localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) =>{
+    const explorer = {name: "Explorer", msg: "Hello"}
+    res.send(explorer)
+})
+
+//Query Params: Recibir parámetors para la url
+//https://localhost:3000/explorers/John
